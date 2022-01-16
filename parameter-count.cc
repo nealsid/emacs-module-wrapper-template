7e210f9ce4e85419f802d8eddd71c3e57b15f3ab
Neal Sidhwaney <nealsid@users.noreply.github.com>
2022-01-15 21:17:44 -0700

    Add comments and add functionality to test if all optional parameters are trailing.

--- parameter-count.cc
+++ parameter-count.cc
@@ -3,26 +3,37 @@
 
 using namespace std;
 
-template<int NumberOfParameters, int NumberOfOptionalParameters, typename FirstParam, typename... Args>
-struct ParameterTraits : ParameterTraits<NumberOfParameters + 1, NumberOfOptionalParameters, Args...> {};
+template<int NumberOfParameters, int NumberOfOptionalParameters, bool AllOptionalTrailing, typename FirstParam, typename... Args>
+struct ParameterTraits
+  : ParameterTraits<NumberOfParameters + 1, // Increment parameters by 1 for FirstParam
+                    NumberOfOptionalParameters, // Pass thru number of optional parameters
+                    !(NumberOfOptionalParameters > 0), // If NumberOfOptionalParameters > 0,
+                                                       // set this value to false, since we're
+                                                       // in the specialization that handles
+                                                       // non-optional parameters.
+                    Args...> {};
 
-template<int NumberOfParameters, int NumberOfOptionalParameters, typename FirstParameter, typename... Args>
-struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, std::optional<FirstParameter>, Args...>  : ParameterTraits<NumberOfParameters + 1, NumberOfOptionalParameters + 1, Args...> {};
+template<int NumberOfParameters, int NumberOfOptionalParameters, bool AllOptionalTrailing, typename FirstParameter, typename... Args>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, AllOptionalTrailing, optional<FirstParameter>, Args...>
+  : ParameterTraits<NumberOfParameters + 1, NumberOfOptionalParameters + 1, AllOptionalTrailing, Args...> {};
 
-template<int NumberOfParameters, int NumberOfOptionalParameters, typename LastParameter>
-struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, LastParameter> {
+template<int NumberOfParameters, int NumberOfOptionalParameters, bool AllOptionalTrailing, typename LastParameter>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, AllOptionalTrailing, LastParameter> {
   static constexpr size_t parameterCount = NumberOfParameters + 1;
   static constexpr size_t optionalParameterCount = NumberOfOptionalParameters;
+  static constexpr bool allOptionalParametersTrailing = AllOptionalTrailing;
 };
 
 // I tried this without a partial specialization for an optional last
-// parameter, but the function would match both of the previous two
-// specializations (the first one with an empty parameter pack for
-// remaining arguments).
-template<int NumberOfParameters, int NumberOfOptionalParameters, typename LastParameter>
-struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, std::optional<LastParameter>> {
+// parameter (instead, I made the test for optional part of the
+// calculation of optionalParameterCount), but then a function would
+// match both of the previous two specializations (the first one with
+// an empty parameter pack for remaining arguments).
+template<int NumberOfParameters, int NumberOfOptionalParameters, bool AllOptionalTrailing, typename LastParameter>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, AllOptionalTrailing, optional<LastParameter>> {
   static constexpr size_t parameterCount = NumberOfParameters + 1;
   static constexpr size_t optionalParameterCount = NumberOfOptionalParameters + 1;
+  static constexpr bool allOptionalParametersTrailing = AllOptionalTrailing;
 };
 
 template<typename F>
@@ -30,13 +41,13 @@ struct FunctionTraits {};
 
 template<typename R, typename... Args>
 struct FunctionTraits<R(*)(Args...)> {
-  static constexpr ParameterTraits<0, 0, Args...> parameterTraits;
+  static constexpr ParameterTraits<0, 0, true, Args...> parameterTraits;
 };
 
-
-void func(int a, int b, std::optional<int> c);
+void func(int a, int b, optional<int> c, optional<int> d);
 
 int main(int argc, char* argv[]) {
   cout << FunctionTraits<decltype(&func)>::parameterTraits.parameterCount << endl;
   cout << FunctionTraits<decltype(&func)>::parameterTraits.optionalParameterCount << endl;
+  cout << FunctionTraits<decltype(&func)>::parameterTraits.allOptionalParametersTrailing << endl;
 }
