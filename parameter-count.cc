080d0ee41e1b44fd2ee85cf8775a2d2caf7eb0b7
Neal Sidhwaney <nealsid@users.noreply.github.com>
2022-01-15 19:52:48 -0700

    Basic template metaprogram to count parameters and return number of optional arguments.

--- parameter-count.cc
+++ parameter-count.cc
@@ -3,105 +3,40 @@
 
 using namespace std;
 
-// template<int N, int J, typename... Args>
-// struct ParameterCounterBase {
-//   static constexpr int parameterCount = J;
-// };
+template<int NumberOfParameters, int NumberOfOptionalParameters, typename FirstParam, typename... Args>
+struct ParameterTraits : ParameterTraits<NumberOfParameters + 1, NumberOfOptionalParameters, Args...> {};
 
-// template <int N, int J, typename FirstParam, typename... Args>
-// struct ParameterCounterBase : ParameterCounterBase<N-1, J+1, Args...> {};
+template<int NumberOfParameters, int NumberOfOptionalParameters, typename FirstParameter, typename... Args>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, std::optional<FirstParameter>, Args...>  : ParameterTraits<NumberOfParameters + 1, NumberOfOptionalParameters + 1, Args...> {};
 
-// template<typename F>
-// struct ParameterCounterBase {};
-
-// template<int ArgCount, int ArgCounter, typename FirstParam, typename... Args>
-// struct ParameterCounterIter {};
-
-// template<int ArgCounter>
-// struct ParameterCounterIter<0> {};
-
-// template<int ArgCount, int ArgCounter, typename FirstParam, typename...Args>
-// struct ParameterCounterIter : ParameterCounterIter<ArgCount - 1, ArgCounter + 1, Args...> {};
-
-// template <typename R, typename... Args>
-// struct ParameterCounterBase<R(*)(Args...)> : ParameterCounterIter<sizeof...(Args), 0, Args...> {};
-
-// template<auto F>
-// struct ParameterCounter : ParameterCounterBase<decltype(F)> {};
-
-/************** */
-
-template<typename>
-struct is_optional : std::false_type {};
-
-template<typename T>
-struct is_optional<std::optional<T>> : std::true_type {};
-
-template<int CurrentArgCount, int ArgsLeft, typename FirstParam, typename... Args>
-struct CountParameterIter : CountParameterIter<CurrentArgCount + 1, ArgsLeft - 1, Args...> {};
-
-template<int CurrentArgCount, int ArgsLeft, typename FirstParam, typename... Args>
-struct CountParameterIter<CurrentArgCount, ArgsLeft, std::optional<FirstParam>, Args...> : CountParameterIter<CurrentArgCount + 1, ArgsLeft - 1, Args...> {};
-
-template<int CurrentArgCount, typename LastParam>
-struct CountParameterIter<CurrentArgCount, 1, LastParam> {
-  static constexpr size_t argCount = CurrentArgCount + 1;
+template<int NumberOfParameters, int NumberOfOptionalParameters, typename LastParameter>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, LastParameter> {
+  static constexpr size_t parameterCount = NumberOfParameters + 1;
+  static constexpr size_t optionalParameterCount = NumberOfOptionalParameters;
 };
 
-template<int CurrentArgCount, typename LastParam>
-struct CountParameterIter<CurrentArgCount, 1, std::optional<LastParam>> : CountParameterIter<CurrentArgCount, 1, LastParam> {
-  static constexpr size_t optionalParamCount = 1;
+// I tried this without a partial specialization for an optional last
+// parameter, but the function would match both of the previous two
+// specializations (the first one with an empty parameter pack for
+// remaining arguments).
+template<int NumberOfParameters, int NumberOfOptionalParameters, typename LastParameter>
+struct ParameterTraits<NumberOfParameters, NumberOfOptionalParameters, std::optional<LastParameter>> {
+  static constexpr size_t parameterCount = NumberOfParameters + 1;
+  static constexpr size_t optionalParameterCount = NumberOfOptionalParameters + 1;
 };
-
-// template<int CurrentArgCount>
-// struct CountParameterIter<CurrentArgCount, 0> {
-//   static constexpr size_t argCount = CurrentArgCount;
-// };
-  // static constexpr bool onlyTrailingOptionals = true;
-  // static constexpr bool foundOptionalParameter = false;
-
-  // static constexpr std::tuple optionalParams =
-  //   make_tuple((([] () {
-  // 		   if constexpr (std::is_same<is_optional<Args>, std::true_type>::value) {
-  // 		       foundOptionalParameter = true;
-  // 		     } else {
-  // 		     if constexpr (foundOptionalParameter) {
-  // 			 onlyTrailingOptionals = false;
-  // 		       }
-  // 		   }
-  // 		   return ;
-  // 		 }) ()) ...);
-
-template<bool optionalParameters, typename FirstParam, typename... Args>
-struct ParameterTraits : ParameterTraits<optionalParameters, Args...> {};
-
-template<bool optionalParameters, typename LastParameter>
-struct ParameterTraits<optionalParameters, std::optional<LastParameter>> {
-  static constexpr bool foundOptionalParameter = optionalParameters && is_optional<LastParameter>::value;
-};
-
-// template<bool optionalParameters, typename LastParameter>
-// struct ParameterTraits<optionalParameters, LastParameter> {
-
-// };
 
 template<typename F>
 struct FunctionTraits {};
 
 template<typename R, typename... Args>
 struct FunctionTraits<R(*)(Args...)> {
-  static constexpr size_t argSize = sizeof...(Args);
-  static constexpr size_t argCount = CountParameterIter<0, sizeof...(Args), Args...>::argCount;
-  static constexpr size_t optParamCount = CountParameterIter<0, sizeof...(Args), Args...>::optionalParamCount;
-  static constexpr ParameterTraits<false, Args...> parameterTraits;
+  static constexpr ParameterTraits<0, 0, Args...> parameterTraits;
 };
 
 
-void func(int a, std::optional<int> b, std::optional<float> c);
+void func(int a, int b, std::optional<int> c);
 
 int main(int argc, char* argv[]) {
-  cout << FunctionTraits<decltype(&func)>::argCount << endl;
-  cout << FunctionTraits<decltype(&func)>::argSize << endl;
-  cout << FunctionTraits<decltype(&func)>::optParamCount << endl;
-  cout << FunctionTraits<decltype(&func)>::parameterTraits.foundOptionalParameter << endl;
+  cout << FunctionTraits<decltype(&func)>::parameterTraits.parameterCount << endl;
+  cout << FunctionTraits<decltype(&func)>::parameterTraits.optionalParameterCount << endl;
 }
