2597e66a1541b29910d6a2030900d22a5d74b7a9
Neal Sidhwaney <nealsid@users.noreply.github.com>
2022-02-10 17:40:57 -0700

    Add some tests for is_type_any_of<>

--- test/function-traits-test.cc
+++ test/function-traits-test.cc
@@ -35,10 +35,20 @@ static_assert(lf3::numDeallocatedParameters == 1);
 static_assert(lf3::allOptionalParametersTrailing);
 /****/
 
-
+/****/
 template<typename A, typename B>
 struct assert_same_types {
   static_assert(is_same_v<A, B>);
 };
 
 assert_same_types<remove_optional_t<int>, int> a;
+/****/
+
+/****/
+static_assert(is_type_any_of_v<int, int, char*, bool>);
+static_assert(is_type_any_of_v<bool, int, char*, bool>);
+static_assert(!is_type_any_of_v<is_type_any_of<int, char*>, int, char*, bool>);
+static_assert(!is_type_any_of_v<is_type_any_of<int, char*>, is_type_any_of<int, char>>);
+static_assert(is_type_any_of_v<is_type_any_of<int, char*>, is_type_any_of<int, char*>>);
+static_assert(!is_type_any_of_v<int, char*, bool>);
+/****/
